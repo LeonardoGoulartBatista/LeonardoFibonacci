1053cfafac8cd68d3810370bc7d7d7392c0942b7
leonardo goulart batista <leo.canoas4@gmail.com>
2024-05-02 22:49:04 -0300

    Adicionar arquivos de projeto.

---
+++ LeonardoFibonacci/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeonardoFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //0 1 1 2 3 5 8
+            int a = 0;
+            int b = 1;
+            int c = 1;
+
+            Console.Write("quantos valores: ");
+            int valores = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("sequencia de Fibonacci com " + valores + " valores");
+
+            for(int i = 0; i < valores; i++)
+            {
+                if(i < valores - 1)
+                {
+                    Console.Write(a + ", ");
+                }
+                else
+                {
+                    Console.Write(a);
+                }
+                
+
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            Console.ReadKey();
+        }
+    }
+}
